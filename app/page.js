4dc95a3fab25edfe746843630d0018c9631b4748
Roni <164780266+roniabdulaziz@users.noreply.github.com>
2026-05-22 16:45:05 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>Selamat Datang di Aplikasi Pulsa Saya</h1>
+      <p>Aplikasi ini sedang dalam proses pembuatan!</p>
+    </div>
+  )
+  }
